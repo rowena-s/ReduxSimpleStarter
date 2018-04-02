5c8d1df8571f9713d762961dbe445014f1772d22
Rowena Stewart <rowena.stewart@industrieit.com>
2018-04-02 12:18:24 +1000

    end of s2 search bar functioning

--- src/index.js
+++ src/index.js
@@ -4,24 +4,38 @@ import YTSearch from 'youtube-api-search';
 import SearchBar from './components/search_bar';
 import VideoList from './components/video_list';
 import VideoDetail from './components/video_detail';
+import _ from 'Lodash';
 const API_KEY = "";
 
 class App extends Component {
   constructor(props){
     super(props);
 
-    this.state = { videos : [] };
+    this.state = { 
+      videos : [],
+      selectedVideo: null 
+    };
+    this.videoSearch('surfboards');
+  }
 
-    YTSearch({key: API_KEY, term: 'surfboards'}, (videos) => {
-      this.setState({ videos });
+  videoSearch(term){
+    YTSearch({key: API_KEY, term}, (videos) => {
+      this.setState({ 
+        videos,
+        selectedVideo: videos[0]
+       });
     });
   }
+
   render() {
+    const videoSearch = _.debounce((term) => { this.videoSearch(term) }, 300);
     return(
       <div>
-        <SearchBar />
-        <VideoDetail video={this.state.videos[0]}/>
-        <VideoList videos={this.state.videos} />
+        <SearchBar onSearchTermChange={videoSearch} />
+        <VideoDetail video={this.state.selectedVideo}/>
+        <VideoList 
+        onVideoSelect={selectedVideo => this.setState({selectedVideo})}
+        videos={this.state.videos} />
       </div>
     );
   }
